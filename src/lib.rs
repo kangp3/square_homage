2e864a0b199388bcddfc61164a6e9fcac953c78e
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2024-04-16 12:18:22 -0400

    Triangle drawn on a screen

--- src/lib.rs
+++ src/lib.rs
@@ -73,8 +73,60 @@ pub async fn run(clear_color: wgpu::Color) {
         .texture
         .create_view(&wgpu::TextureViewDescriptor::default());
 
+    let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl")); //TODO: Break this string out to be called by main
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        //copy pasted from learnWGPU. This is verbose
+        label: Some("Render Pipeline"),
+        layout: Some(&render_pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState {
+                    color: wgpu::BlendComponent::REPLACE,
+                    alpha: wgpu::BlendComponent::REPLACE,
+                }),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+            // or Features::POLYGON_MODE_POINT
+            polygon_mode: wgpu::PolygonMode::Fill,
+            // Requires Features::DEPTH_CLIP_CONTROL
+            unclipped_depth: false,
+            // Requires Features::CONSERVATIVE_RASTERIZATION
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        // If the pipeline will be used with a multiview render pass, this
+        // indicates how many array layers the attachments will have.
+        multiview: None,
+    });
+
     {
-        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("Render Pass"),
             color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                 view: &view,
@@ -88,12 +140,14 @@ pub async fn run(clear_color: wgpu::Color) {
             occlusion_query_set: None,
             timestamp_writes: None,
         });
+        render_pass.set_pipeline(&render_pipeline); // 2.
+        render_pass.draw(0..3, 0..1); // 3.
     }
 
     queue.submit(iter::once(encoder.finish()));
     output.present();
 
-    event_loop.run(move |event, window| handle_event(event, window));
+    let _ = event_loop.run(move |event, window| handle_event(event, window));
 }
 
 fn handle_event(event: Event<()>, window: &EventLoopWindowTarget<()>) {
